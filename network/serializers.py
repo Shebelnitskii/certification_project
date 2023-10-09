a91899e7972dd089f10fa1839aaf8dcd01c348ce
shebel <Shebelnitskiy@gmail.com>
2023-10-09 16:17:04 +0400

    Добавление в сериализаторе запрет на изменение задолжености

--- network/serializers.py
+++ network/serializers.py
@@ -7,3 +7,8 @@ class NetworkNodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = NetworkNode
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        if 'debt' in validated_data:
+            raise serializers.ValidationError("Нельзя изменять задолженность.")
+        return super().update(instance, validated_data)
