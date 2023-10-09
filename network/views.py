5029721553976378937445d495a285e006c64d62
shebel <Shebelnitskiy@gmail.com>
2023-10-09 16:16:16 +0400

    Исправление урлов CRUDa

--- network/views.py
+++ network/views.py
@@ -11,6 +11,14 @@ class NetworkNodeListAPI(generics.ListAPIView):
     queryset = NetworkNode.objects.all()
     serializer_class = NetworkNodeSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        country = self.request.query_params.get('country')
+        if country:
+            # Фильтруем объекты по стране
+            queryset = queryset.filter(contact__country=country)
+        return queryset
+
 
 class NetworkNodeCreateAPI(generics.CreateAPIView):
     queryset = NetworkNode.objects.all()
