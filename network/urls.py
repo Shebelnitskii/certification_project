df7d584776189b7baf73cadbab4fd7c7e181e563
shebel <Shebelnitskiy@gmail.com>
2023-10-09 15:39:47 +0400

    Добавление урлов CRUDa

--- network/urls.py
+++ network/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
+from network.views import NetworkNodeListAPI, NetworkNodeCreateAPI, NetworkNodeDetailAPI, NetworkNodeUpdateAPI, \
+    NetworkNodeDeleteAPI
+
 app_name = 'network'
 
 urlpatterns = [
+    path('networknode/', NetworkNodeListAPI.as_view(), name='networknode-list'),
+    path('networknode/create/', NetworkNodeCreateAPI.as_view(), name='networknode-create'),
+    path('networknode/detail/<int:pk>/', NetworkNodeDetailAPI.as_view(), name='networknode-detail'),
+    path('networknode/delete/<int:pk>/', NetworkNodeUpdateAPI.as_view(), name='networknode-delete'),
+    path('networknode/update/<int:pk>/', NetworkNodeDeleteAPI.as_view(), name='networknode-update'),
 ]
