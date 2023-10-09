412825b7b91adb918171e1646fbf88c21e0de053
shebel <Shebelnitskiy@gmail.com>
2023-10-09 16:16:31 +0400

    Исправление урлов CRUDa

--- network/urls.py
+++ network/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('networknode/', NetworkNodeListAPI.as_view(), name='networknode-list'),
     path('networknode/create/', NetworkNodeCreateAPI.as_view(), name='networknode-create'),
     path('networknode/detail/<int:pk>/', NetworkNodeDetailAPI.as_view(), name='networknode-detail'),
-    path('networknode/delete/<int:pk>/', NetworkNodeUpdateAPI.as_view(), name='networknode-delete'),
-    path('networknode/update/<int:pk>/', NetworkNodeDeleteAPI.as_view(), name='networknode-update'),
+    path('networknode/delete/<int:pk>/', NetworkNodeDeleteAPI.as_view(), name='networknode-delete'),
+    path('networknode/update/<int:pk>/', NetworkNodeUpdateAPI.as_view(), name='networknode-update'),
 ]
