c850ed824ece47fbd5375edd3df3d605a0e61f16
shebel <Shebelnitskiy@gmail.com>
2023-10-09 15:00:45 +0400

    Создание костомной команды юзеров, и обновление урлов

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('network.urls', namespace='network'))
+    path('', include('network.urls', namespace='network')),
+    path('users', include('users.urls', namespace='users'))
 ]
