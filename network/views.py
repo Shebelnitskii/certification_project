61c047b8422d79ffcf92a3fa70981dc3f6764374
shebel <Shebelnitskiy@gmail.com>
2023-10-09 15:20:49 +0400

    Реализация базового CRUD

--- network/views.py
+++ network/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import generics
+from network.models import NetworkNode
+from network.serializers import NetworkNodeSerializer
+
 
 # Create your views here.
+
+
+class NetworkNodeListAPI(generics.ListAPIView):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+
+
+class NetworkNodeCreateAPI(generics.CreateAPIView):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+
+
+class NetworkNodeDetailAPI(generics.RetrieveAPIView):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+
+
+class NetworkNodeUpdateAPI(generics.UpdateAPIView):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+
+
+class NetworkNodeDeleteAPI(generics.DestroyAPIView):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
