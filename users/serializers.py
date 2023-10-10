51ba4174b7fd2bae69416c809dd57d7c1f53eefd
shebel <Shebelnitskiy@gmail.com>
2023-10-10 15:39:16 +0400

    Обновление сериализатора (добавление метода хеширования пароля при создании или изменении пользователя)

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ('id', 'email', 'phone', 'first_name', 'last_name', 'is_active', 'is_staff')
+        fields = ('id', 'email', 'password', 'phone', 'is_active', 'is_staff')
+
+    def create(self, validated_data):
+        """ Хеширование пароля при создании нового пользователя в БД """
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        """ Хеширование пароля при изменении пароля пользователя """
+        # Проверяем, есть ли поле "password" в validated_data
+        if 'password' in validated_data:
+            # Если есть, хешируем новый пароль
+            validated_data['password'] = make_password(validated_data['password'])
+        return super().update(instance, validated_data)
