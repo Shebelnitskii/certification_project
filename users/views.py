70c27ea0e7ee9ee258ab5a2a7a31454063f3adbb
shebel <Shebelnitskiy@gmail.com>
2023-10-10 15:24:03 +0400

    Создание сериализатора для User, реализация CRUD для пользователей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class UserListAPI(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailAPI(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserCreateAPI(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserUpdateAPI(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDeleteAPI(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
